a237f51a8c79626d818c16a0b264602d049e2377
adrianre12 <adrianrevill@hotmail.com>
2025-06-14 09:03:59 +0100

    Inital copy from SETest clean

---
+++ Log.cs
@@ -0,0 +1,21 @@
+﻿using VRage.Utils;
+
+namespace SEtest
+{
+    public static class Log
+    {
+        const string Prefix = "SETest";
+
+        public static bool DebugLog;
+        public static void Msg(string msg)
+        {
+            MyLog.Default.WriteLine($"{Prefix}: {msg}");
+        }
+
+        public static void Debug(string msg)
+        {
+            if (DebugLog)
+                Msg($"[DEBUG] {msg}");
+        }
+    }
+}
---
+++ TestBlock.cs
@@ -0,0 +1,50 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+
+namespace SEtest
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_VirtualMass), false, new[] { "TestBlockLarge", "TestBlockSmall" })]
+    //[MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), false)]
+    internal class TestBlock : MyGameLogicComponent
+    {
+        private IMyFunctionalBlock block;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            if (!MyAPIGateway.Session.IsServer)
+                return;
+
+            block = Entity as IMyFunctionalBlock;
+
+            NeedsUpdate = MyEntityUpdateEnum.EACH_100TH_FRAME;
+
+        }
+
+        public override void UpdateAfterSimulation100()
+        {
+            Log.Msg($"Tick {block.CubeGrid.DisplayName}");
+        }
+
+        public override void OnAddedToScene()
+        {
+            base.OnAddedToScene();
+            Log.Msg($"OnAddedToScene {block.CubeGrid.DisplayName}");
+        }
+
+        public override void OnRemovedFromScene()
+        {
+            Log.Msg($"OnRemovedFromScene {block.CubeGrid.DisplayName}");
+
+        }
+
+        public override void MarkForClose()
+        {
+            base.MarkForClose();
+            Log.Msg($"MarkForClose {block.CubeGrid.DisplayName}");
+
+        }
+    }
+}
