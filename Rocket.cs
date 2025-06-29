ac86646397bfc0f373a65e9aa516b9e3f1ac8f30
adrianre12 <adrianrevill@hotmail.com>
2025-06-29 15:26:26 +0100

    Added basic respawn

--- Rocket.cs
+++ Rocket.cs
@@ -16,16 +16,16 @@ namespace SEtest
         private MyCubeGrid grid;
         private float rotationSpeed = 0.05f;
         private float targetSpeed = 1f ;
-        private float deceleration = 0.49f;
         private MyPlanet closestPlanet;
         private double height;
         private bool active;
-        private double deployHeight = 15;
+        private double deployHeight = 200;
         private Vector3 up;
         private Quaternion to;
         private Quaternion from;
         private bool landed = false;
         private Vector3 gravity;
+        private float decreaseStep;
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -51,11 +51,9 @@ namespace SEtest
             if (closestPlanet != null)
                 height = closestPlanet.GetHeightFromSurface(grid.WorldMatrix.Translation);
             block.Enabled = true;
-            //block.ThrustOverridePercentage = 0.01f;
-            Log.Msg($"Start height={height} override={block.ThrustOverridePercentage} currentThrust={block.CurrentThrust}  dampeners={grid.DampenersEnabled}");
+            //Log.Msg($"Start height={height} override={block.ThrustOverridePercentage} currentThrust={block.CurrentThrust}  dampeners={grid.DampenersEnabled}");
 
             gravity = grid.NaturalGravity;
-            deceleration = gravity.Length() / 2;
 
             Log.Msg($"Gravity={gravity.Length()}");
 
@@ -70,7 +68,7 @@ namespace SEtest
                 return;
             if (active && grid.Physics.Speed > 0.1)
             {
-                up = -gravity;// -grid.Physics.LinearVelocity;            
+                up = -gravity;           
                 from = Quaternion.CreateFromForwardUp(grid.WorldMatrix.Forward, grid.WorldMatrix.Up);
 
                 var newMatrix = MatrixD.CreateFromQuaternion( Quaternion.Slerp(from, from * Quaternion.CreateFromTwoVectors(grid.WorldMatrix.Up, up), rotationSpeed));
@@ -80,9 +78,9 @@ namespace SEtest
 
                 var speed = grid.Physics.Speed;
                 var multiplier = targetSpeed / speed;
-                if (speed - deceleration > targetSpeed)
-                    multiplier = (speed - deceleration) / speed;
 
+                if (speed - decreaseStep > targetSpeed)
+                    multiplier = (speed - decreaseStep) / speed;
                 grid.Physics.LinearVelocity = grid.Physics.LinearVelocity * multiplier;
             }
         }
@@ -98,18 +96,21 @@ namespace SEtest
             if (closestPlanet != null )
                 height = closestPlanet.GetHeightFromSurface(grid.WorldMatrix.Translation);
             active = (height < deployHeight && height > 1);
-            if (active)
+            if (active) 
                 block.ThrustOverridePercentage = 1f;
-            else
+            else 
+            {
                 block.ThrustOverridePercentage = 0;
+                decreaseStep = grid.Physics.Speed/150;
+            }
 
             if (height < 1 || grid.Physics.Speed < 0.05)
             {
-                Log.Msg($"Landed height={height} speed={grid.Physics.Speed}");
+               // Log.Msg($"Landed height={height} speed={grid.Physics.Speed}");
                 landed = true;
                 block.Enabled = false;
             }
-            Log.Msg($"height={height} active={active} speed={grid.Physics.Speed} override={block.ThrustOverridePercentage} currentThrust={block.CurrentThrust} dampners={grid.DampenersEnabled}");
+            //Log.Msg($"height={height} active={active} speed={grid.Physics.Speed} override={block.ThrustOverridePercentage} currentThrust={block.CurrentThrust} dampners={grid.DampenersEnabled}");
             
         }
     }
