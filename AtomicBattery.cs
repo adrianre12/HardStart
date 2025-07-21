03a563efd2e5def605ef21622c5a256e8df86cc9
adrianre12 <adrianrevill@hotmail.com>
2025-07-21 13:38:42 +0100

    Fix skybox transparancy

--- AtomicBattery.cs
+++ AtomicBattery.cs
@@ -1,4 +1,5 @@
 ﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game;
 using Sandbox.Game.Entities.Blocks;
 using Sandbox.ModAPI;
 using System;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VRage;
+using VRage.Game;
 using VRage.Game.Components;
 using VRage.Game.Entity;
 using VRage.Game.ModAPI;
@@ -19,8 +21,11 @@ namespace HardStart
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Reactor), false, new[] { "EmergencySmallGenerator" })]
     internal class AtomicBattery : MyGameLogicComponent
     {
-        private IMyCubeBlock block;
-        private List<VRage.Game.ModAPI.Ingame.MyInventoryItem> inventory = new List<VRage.Game.ModAPI.Ingame.MyInventoryItem>();
+        private IMyTerminalBlock block;
+        private float tritiumAmount;
+        private MyInventory inventory;
+        private MyDefinitionId id = new MyDefinitionId(typeof(MyObjectBuilder_Component), "EmergencyTritiumCell");
+
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -32,62 +37,73 @@ namespace HardStart
                  return;
             //Log.Msg($"IsServer={MyAPIGateway.Session.IsServer} IsDedicated={MyAPIGateway.Utilities.IsDedicated}");
 
-            block = Entity as IMyCubeBlock;
+            block = Entity as IMyTerminalBlock;
 
             NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
 
         }
 
+        void AppendingCustomInfo(IMyTerminalBlock block, StringBuilder sb)
+        {
+            try 
+            {
+                sb.Append($"Tritium Cell: {(tritiumAmount*100):0.00}%\n");
+            }
+            catch (Exception e)
+            {
+                Log.Msg(e.Message);
+            }
+        }
+
         public override void UpdateOnceBeforeFrame()
         {
-            //Log.Msg("AB UpdateOnceBeforeFrame");
             base.UpdateOnceBeforeFrame();
             if (block.CubeGrid?.Physics == null)
                 return;
 
+            inventory = block.GetInventory() as MyInventory;
+
             CheckTCellVisability();
+
+            block.AppendingCustomInfo += AppendingCustomInfo;
             NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
         }
 
         public override void UpdateAfterSimulation100()
         {
             base.UpdateAfterSimulation100();
-            //Log.Msg("UpdateAfterSimulation100");
             CheckTCellVisability();
+
+            try { 
+                if (MyAPIGateway.Gui.GetCurrentScreen == MyTerminalPageEnum.ControlPanel)
+                {
+                    block.RefreshCustomInfo();
+                    block.SetDetailedInfoDirty();
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Msg(e.Message);
+            }
         }
 
         private void CheckTCellVisability()
         {
-            //Log.Msg("CheckTCellVisability");
-            VRage.Game.ModAPI.Ingame.MyItemType tcellType = VRage.Game.ModAPI.Ingame.MyItemType.MakeComponent("EmergencyTritiumCell");
-            if (block.GetInventory().ContainItems((MyFixedPoint)0.001, tcellType))
-            {
-                //Log.Msg("found Tritium");
-                UpdateTCellVisability(true);
-            }
-            else
-            {
-                //Log.Msg("no Tritium");
-                UpdateTCellVisability(false);
-            }
-        }
+            tritiumAmount = (float)inventory.GetItemAmount(id);
 
-        private void UpdateTCellVisability(bool visible)
-        {
-            //Log.Msg("UpdateTCellVisability");
             try
             {
                 MyEntitySubpart subpart;
                 if (Entity.TryGetSubpart("TritiumCell", out subpart)) // subpart does not exist when block is in build stage
                 {
-                    subpart.Render.Visible = visible;
+                    subpart.Render.Visible = tritiumAmount > 0.001;
                 }
             }
             catch (Exception e)
             {
                 Log.Msg(e.ToString());
             }
+        }
     }
 }
-    }
 
